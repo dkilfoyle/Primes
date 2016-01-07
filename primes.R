c58f3a96ab65f7ba032a1fa263b9c83b361705dd
dkilfoyle <dkilfoyle@xtra.co.nz>
2016-01-08 00:50:02 +1300

    First upload

---
+++ primes.R
@@ -0,0 +1,42 @@
+getPrimesUpToNBySieve = function(n)
+{
+  n = as.integer(n)
+  if (n > 1e8)
+    stop("n too large")
+  primes = rep(TRUE, n)
+  primes[1] = FALSE
+  last.prime = 2L
+  fsqr = floor(sqrt(n))
+  while (last.prime <= fsqr)
+  {
+    primes[seq.int(2L*last.prime, n, last.prime)] = FALSE
+    sel = which(primes[(last.prime+1):(fsqr+1)])
+    if(any(sel)){
+      last.prime = last.prime + min(sel)
+    }else last.prime = fsqr+1
+  }
+  which(primes)
+}
+
+PrimePi = function(n) {
+  length(getPrimesUpToNBySieve(n))
+}
+
+PrimePiList = function(n) {
+  n = as.integer(n)
+  if (n > 1e8)
+    stop("n too large")
+  primes = rep(TRUE, n)
+  primes[1] = FALSE
+  last.prime = 2L
+  fsqr = floor(sqrt(n))
+  while (last.prime <= fsqr)
+  {
+    primes[seq.int(2L*last.prime, n, last.prime)] = FALSE
+    sel = which(primes[(last.prime+1):(fsqr+1)])
+    if(any(sel)){
+      last.prime = last.prime + min(sel)
+    }else last.prime = fsqr+1
+  }
+  cumsum(primes)
+}
